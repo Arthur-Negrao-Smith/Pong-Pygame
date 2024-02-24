a840a3f79da0631ddc59b15eff46b480e492c881
Arthur Negrão <arthurnegrao10s@gmail.com>
2024-02-23 21:08:24 -0300

    Criando Seletor de Direção

--- main.py
+++ main.py
@@ -15,14 +15,22 @@ game_screen.create_display()
 player_size_x = 30
 player_size_y = 100
 
-# Criando os Players
-player_1 = Rect_player(posx=0, posy=screen_size_y//2, widith=player_size_x, height=player_size_y, color='green')
-player_2 = Rect_player(posx=screen_size_x - player_size_x, posy=screen_size_y//2, widith=player_size_x, height=player_size_y, color='red')
-
-# Controla a velocidade do jogo
+# Controla as velocidades do jogo
 player_speed = 5
-ball_speed = 0
+ball_speed = 5
 
+# Criando os Players
+player_1 = Rect_player(posx=0, posy=screen_size_y//2, widith=player_size_x, height=player_size_y, color='green', speed=player_speed)
+player_2 = Rect_player(posx=screen_size_x - player_size_x, posy=screen_size_y//2, widith=player_size_x, height=player_size_y, color='red', speed=ball_speed)
+
+# Características da bola
+ray = 10
+ball_color = 'purple'
+
+# Criando a bola
+ball = Ball(ray=ray, posx=screen_size_x//2, posy=screen_size_y//2, color=ball_color, speed=ball_speed)
+
+# Relógio do jogo
 clock = pg.time.Clock()
 
 # Identificador de eventos
@@ -39,11 +47,15 @@ while running:
 
     player_1.draw_player(screen=game_screen.game_window)
     if game_event.identify_key_up() and player_1.posy >= 0:
-        player_1.move_up(speed=player_speed)
+        player_1.move_up()
     if game_event.identify_key_down() and player_1.posy <= screen_size_y - player_size_y:
-        player_1.move_down(speed=player_speed)
+        player_1.move_down()
 
     player_2.draw_player(screen=game_screen.game_window)
+
+    ball.draw_ball(screen=game_screen.game_window)
+    
+
 
     pg.display.flip()
 
--- pong_func.py
+++ pong_func.py
@@ -36,18 +36,19 @@ class Game_Event:
             return True
 
 class Rect_player:
-    def __init__(self, posx, posy, widith, height, color) -> None:
+    def __init__(self, posx, posy, widith, height, color, speed) -> None:
         self.posx = posx
         self.posy = posy
         self.xsize = widith
         self.ysize = height
         self.color= color
-        
-    def move_up(self, speed) -> None:
-        self.posy -= speed
+        self.speed = speed
 
-    def move_down(self, speed) -> None:
-        self.posy += speed
+    def move_up(self) -> None:
+        self.posy -= self.speed
+
+    def move_down(self) -> None:
+        self.posy += self.speed
         
     def return_x_pos(self) -> int:
         return self.posx
@@ -59,10 +60,47 @@ class Rect_player:
         pg.draw.rect(color=self.color, surface=screen, rect=((self.posx, self.posy), (self.xsize, self.ysize)))
 
 class Ball:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, ray, posx, posy, color, speed) -> None:
+        self.ray = ray
+        self.posx = posx
+        self.posy = posy
+        self.color = color
+        self.speed = speed
 
-    
+    def move_ball_up_right(self):
+        self.posx += self.speed
+        self.posy -= self.speed
+
+    def move_ball_up_left(self):
+        self.posx -= self.speed
+        self.posy -= self.speed
+
+    def move_ball_down_right(self):
+        self.posx += self.speed
+        self.posy += self.speed
+
+    def move_ball_down_left(self):
+        self.posx -= self.speed
+        self.posy += self.speed
+
+    def draw_ball(self, screen):
+        pg.draw.circle(color=self.color, surface=screen, center=[self.posx, self.posy], radius=self.ray)
+        
+    def select_direction(self, screen):
+        from random import randint
+        if self.posx == 0 or self.posx == screen - self.ray*2:
+            selector = randint(1, 4)
+            if selector == 1:
+                self.move_ball_down_left()
+            if selector == 2:
+                self.move_ball_down_right()
+            if selector == 3:
+                self.move_ball_up_left()
+            if selector == 4:
+                self.move_ball_up_right()
+            return False
+        return True
+
 class Score:
     def __init__(self, points=int) -> None:
         self.points = points
